396d59d43025ab0bf64dbff34ba7f2ba875c6da8
julioreisdev <juliosteam0352@gmail.com>
2022-08-09 14:21:54 -0300

    Adiciona layout de signup versão desktop

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import SignUp from "./SignUp/SignUp";
+import "../assets/css/reset.css";
+import "../assets/css/style.css";
 
 export default function App() {
   return (
