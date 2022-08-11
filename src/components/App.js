0010a455ba1a57e64a6f8f01e3badf0fde18e9e8
Victor Barros <victorwbarros@gmail.com>
2022-08-11 02:49:51 -0300

    Código reorganizado e Refatorado

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,14 @@
-import TimelinePage from "./timelineRoute/timelinePage.js";
 
 import elementStatusContext from "../context/ElementsStatus.js";
 import { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
 import UserContext from "../contexts/UserContext";
 
-import SignUp from "./SignUp";
-import Login from "./Login";
+
+import SignUp from "./authRoute/SignUp";
+import Login from "./authRoute/Login";
+import TimelinePage from "./timelineRoute/timelinePage.js";
+import HashtagPage from "./hashtagPage/HashtagPage.js";
 import avatar from "../assets/images/linkr_Logo.png";
 export default function App() {
   const [userdata, setUserdata] = useState("");
@@ -31,6 +32,7 @@ export default function App() {
             <Route path="/" element={<Login />} />
             <Route path="/sign-up" element={<SignUp />} />
             <Route path="/timeline" element={<TimelinePage />} />
+            <Route path="/hashtag/:hashtag" element={<HashtagPage/>}/>  
           </Routes>
         </BrowserRouter>
       </elementStatusContext.Provider>
